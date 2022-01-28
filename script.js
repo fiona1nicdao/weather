e06f183759e2421bcc33a8d9e1edc48e5f6ba680
Fiona Nicdao <fionanicdao@dhcp-10-102-51-45.wireless.northwestern.private>
2022-01-28 10:11:10 -0600

    remove consolelog

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ btnSearchEl.addEventListener("click",searchTextbox);
 function searchTextbox(event) {
     event.preventDefault()
     cityName = inputField.value
-    console.log(cityName)
     fetchData() 
 }
 // austin button
@@ -28,62 +27,53 @@ btnAusEl.addEventListener("click", austin)
 function austin() {
     // location.reload()
     cityName = 'Austin' 
-    console.log(cityName)
     fetchData()   
 }
 // chicago button
 btnChiEl.addEventListener("click", chicago)
 function chicago() {
     cityName = 'Chicago'
-    console.log(cityName)
     fetchData()   
 }
 // new york button
 btnNewEl.addEventListener("click", newyork)
 function newyork() {
     cityName = 'New York'
-    console.log(cityName)
     fetchData()   
 }
 // orlando button
 btnOrlEl.addEventListener("click", orlando)
 function orlando() {
     cityName = 'Orlando'
-    console.log(cityName)
     fetchData()   
 }
 // san francisco button
 btnSanEl.addEventListener("click", sanfrancisco)
 function sanfrancisco() {
     cityName = 'San Francisco'
-    console.log(cityName)
     fetchData()   
 }
 // seattle button
 btnSeaEl.addEventListener("click", seattle)
 function seattle() {
     cityName = 'Seattle'
-    console.log(cityName)
     fetchData()   
 }
 // denver button
 btnDenEl.addEventListener("click", denver)
 function denver() {
     cityName = 'Denver'
-    console.log(cityName)
     fetchData()  
 }
 // atlanta button
 btnAtlEl.addEventListener("click", atlanta)
 function atlanta() {
     cityName = 'Atlanta'
-    console.log(cityName)
     fetchData()   
 }
 // fetch data 
 function fetchData() {
     document.getElementById('mainsection').classList.remove("mainpart")
-
     document.getElementById("city").remove()
     var apiKey = 'b1d3856bac61869cb925c991abf62e2c'
     requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&units=imperial' + '&appid=' + apiKey
@@ -93,14 +83,11 @@ function fetchData() {
         return response.json();
     })
     .then(function(weatherData){
-        console.log(weatherData)
         var lat = weatherData.coord.lat
-        console.log("lat",lat)
         localStorage.setItem("lat",JSON.stringify(lat))
 
         var lon = weatherData.coord.lon
         localStorage.setItem("lon",JSON.stringify(lon))
-        console.log("lon",lon)
 
         // city name
         var nameCity = document.createElement('h2')
@@ -115,7 +102,6 @@ function fetchData() {
         timeEl.textContent = time
         mainCity.append(timeEl)
         timeEl.classList.add("line")
-        console.log(time)
 
         // weather icon
         var weatherIcon = document.createElement('img')
@@ -147,18 +133,14 @@ function fetchData() {
 function moreFetch() {
     // UV index
     var lat2 = JSON.parse(localStorage.getItem("lat"))
-    console.log("lat2",lat2)
     var lon2 = JSON.parse(localStorage.getItem("lon"))
-    console.log("lon2",lon2)
     var apiKey = 'b1d3856bac61869cb925c991abf62e2c'
     var uvURL = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat2 +"&lon="+lon2 +"&exclude=hourly&units=imperial&appid=" +apiKey
-    console.log(uvURL)
     fetch(uvURL)
     .then(function(response){
         return response.json();
     })
     .then(function(uvData){
-        console.log(uvData)
         var uvEl = document.createElement('p')
         var uv = uvData.current.uvi
         uvEl.textContent = "UV Index: " + uv
@@ -166,7 +148,6 @@ function moreFetch() {
         mainCity.appendChild(uvEl)
         uvEl.classList.add("uvStyle")
         var uvSyleEl = document.querySelector(".uvStyle")
-        console.log(typeof uv)
         if (uv <= 2.99) {
             uvSyleEl.setAttribute("style", "background-color: #008000")
         } else if (uv >= 3 && uv <= 7.99) {
